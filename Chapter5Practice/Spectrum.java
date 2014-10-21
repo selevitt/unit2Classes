a1853e7440dad0fd9b427ca2ae6bc51b67421585
selevitt <selevitt@student.naperville203.org>
2014-10-21 15:01:52 -0500

    Chapter 5 practice

--- Chapter5Practice/Spectrum.java
+++ Chapter5Practice/Spectrum.java
@@ -1,40 +1,8 @@
 
-
-/**
- * Write a description of class Spectrum here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Spectrum
 {
-    /** description of instance variable x (add comment for each instance variable) */
-    private int x;
-
-    /**
-     * Default constructor for objects of class Spectrum
-     */
-    public Spectrum()
+    public static void main(String[] args)
     {
-        // initialise instance variables
-        x = 0;
+        System.out.println("");
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *    that describes the operation of the method
-     *
-     * @pre        preconditions for the method
-     *            (what the method assumes about the method's parameters and class's state)
-     * @post    postconditions for the method
-     *            (what the method guarantees upon completion)
-     * @param    y    description of parameter y
-     * @return    description of the return value
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x+y;
-    }
-
 }
